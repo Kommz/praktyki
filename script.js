50e101b07f4f230925f63151bd3ece47f9c8134f
Kommz <94364990+Kommz@users.noreply.github.com>
2024-02-29 20:11:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const zmienna1 = document.querySelector('.short-title').innerHTML;
+const price = document.querySelector('#jana2v->a>ins').innerHTML;
+const zmienna2 = document.querySelector('h3>.elprpa--link').innerHTML;
+const price2 = document.querySelector('#elprpa->a>ins').innerHTML;
+const zmienna3 = document.querySelector('h3>.styel2--link').innerHTML;
+const price3 = document.querySelector('#styel2->a>ins').innerHTML;
+console.log(zmienna1)
+console.log(zmienna2)
+console.log(zmienna3)
+console.log(price)
+console.log(price2)
+console.log(price3)
